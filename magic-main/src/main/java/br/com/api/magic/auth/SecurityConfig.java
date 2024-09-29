974ec7df1947c876d2fdfcb3287d7bc96dece410
Matheus Toscano Rossini <matheustosros@gmail.com>
2024-09-28 21:54:15 -0300

    rotas adicionais

--- magic-main/src/main/java/br/com/api/magic/auth/SecurityConfig.java
+++ magic-main/src/main/java/br/com/api/magic/auth/SecurityConfig.java
@@ -31,6 +31,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.GET, "/card/commander").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET, "/deck/allDecks").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/deck/login/userDeck").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/deck/send/deck").permitAll()
                         .anyRequest().authenticated()
                 ) 
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
