6e1207a47987b41376f2fc1f6efe37531aaa0799
Toscano <matheustosros@gmail.com>
2024-12-01 17:14:54 -0300

    ajustes nas services

--- magic-main/src/main/java/br/com/api/magic/service/NotificationWorkerService.java
+++ magic-main/src/main/java/br/com/api/magic/service/NotificationWorkerService.java
@@ -1,19 +1,30 @@
 package br.com.api.magic.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationWorkerService {
 
-    @Autowired
-    private SimpMessagingTemplate messagingTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(NotificationWorkerService.class);
+    private static final String WEBSOCKET_DESTINATION = "/topic/deckUpdates";
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public NotificationWorkerService(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
 
     @RabbitListener(queues = "deck_updates_queue")
     public void notifyDeckUpdate(String updateMessage) {
-        messagingTemplate.convertAndSend("/topic/deckUpdates", updateMessage);
-        System.out.println("Mensagem enviada para WebSocket: " + updateMessage);
+        try {
+            messagingTemplate.convertAndSend(WEBSOCKET_DESTINATION, updateMessage);
+            logger.info("Mensagem enviada para WebSocket: {}", updateMessage);
+        } catch (Exception e) {
+            logger.error("Erro ao enviar mensagem para WebSocket: {}", e.getMessage(), e);
+        }
     }
 }
--- magic-main/src/main/java/br/com/api/magic/service/ProducerService.java
+++ magic-main/src/main/java/br/com/api/magic/service/ProducerService.java
@@ -1,25 +1,43 @@
 package br.com.api.magic.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProducerService {
 
-    @Autowired
-    private AmqpTemplate amqpTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(ProducerService.class);
 
-    private final String QUEUE_NAME = "notificationQueue";
-    private final String UPDATE_QUEUE_NAME = "deck_updates_queue";
+    private final AmqpTemplate amqpTemplate;
+
+    @Value("${rabbitmq.queue.notification:notificationQueue}")
+    private String notificationQueueName;
+
+    @Value("${rabbitmq.queue.deckUpdates:deck_updates_queue}")
+    private String deckUpdatesQueueName;
+
+    public ProducerService(AmqpTemplate amqpTemplate) {
+        this.amqpTemplate = amqpTemplate;
+    }
 
     public void sendDeckUpdateMessage(String message) {
-        amqpTemplate.convertAndSend(UPDATE_QUEUE_NAME, message);
-        System.out.println("Mensagem de atualização enviada: " + message);
+        try {
+            amqpTemplate.convertAndSend(deckUpdatesQueueName, message);
+            logger.info("Mensagem de atualização enviada para a fila '{}': {}", deckUpdatesQueueName, message);
+        } catch (Exception e) {
+            logger.error("Erro ao enviar mensagem de atualização: {}", e.getMessage(), e);
+        }
     }
 
     public void sendNotification(String message) {
-        amqpTemplate.convertAndSend(QUEUE_NAME, message);
-        System.out.println("Mensagem de notificação enviada: " + message);
+        try {
+            amqpTemplate.convertAndSend(notificationQueueName, message);
+            logger.info("Mensagem de notificação enviada para a fila '{}': {}", notificationQueueName, message);
+        } catch (Exception e) {
+            logger.error("Erro ao enviar mensagem de notificação: {}", e.getMessage(), e);
+        }
     }
 }
