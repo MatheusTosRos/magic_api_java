f9599dda198186f025cefd3e4da2d0af63674208
Toscano <matheustosros@gmail.com>
2024-12-01 17:16:44 -0300

    subindo docker 2.0

--- magic-main/src/main/java/br/com/api/magic/worker/DeckUpdateWorker.java
+++ magic-main/src/main/java/br/com/api/magic/worker/DeckUpdateWorker.java
@@ -1,20 +1,33 @@
 package br.com.api.magic.worker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DeckUpdateWorker {
 
-    @Autowired
-    private SimpMessagingTemplate messagingTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(DeckUpdateWorker.class);
+    private static final String WEBSOCKET_DESTINATION = "/topic/deck-updates";
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public DeckUpdateWorker(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
 
     @RabbitListener(queues = "deck_updates_queue")
     public void handleDeckUpdate(String message) {
-        System.out.println("Notificação de atualização do baralho: " + message);
+        try {
+            logger.info("Mensagem recebida na fila 'deck_updates_queue': {}", message);
 
-        messagingTemplate.convertAndSend("/topic/deck-updates", message);
+            messagingTemplate.convertAndSend(WEBSOCKET_DESTINATION, message);
+            logger.info("Mensagem enviada ao WebSocket no destino '{}': {}", WEBSOCKET_DESTINATION, message);
+
+        } catch (Exception e) {
+            logger.error("Erro ao processar mensagem de atualização do baralho: {}", e.getMessage(), e);
+        }
     }
 }
