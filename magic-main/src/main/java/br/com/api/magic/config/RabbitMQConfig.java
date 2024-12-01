702a441abb0a400e08fcc685134df8bba04d25a1
Toscano <matheustosros@gmail.com>
2024-12-01 17:13:57 -0300

    ajustes de cpnfiguração

--- magic-main/src/main/java/br/com/api/magic/config/RabbitMQConfig.java
+++ magic-main/src/main/java/br/com/api/magic/config/RabbitMQConfig.java
@@ -1,26 +1,34 @@
 package br.com.api.magic.config;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.amqp.core.Queue;
 
 @Configuration
 public class RabbitMQConfig {
 
+    @Value("${rabbitmq.queue.notification:notificationQueue}")
+    private String notificationQueueName;
+
+    @Value("${rabbitmq.queue.deckImport:deck_import_queue}")
+    private String deckImportQueueName;
+
+    @Value("${rabbitmq.queue.deckUpdates:deck_updates_queue}")
+    private String deckUpdatesQueueName;
+
     @Bean
     public Queue notificationQueue() {
-        return new Queue("notificationQueue", true);
+        return new Queue(notificationQueueName, true);
     }
 
     @Bean
     public Queue deckImportQueue() {
-        return new Queue("deck_import_queue", true);
+        return new Queue(deckImportQueueName, true);
     }
 
     @Bean
     public Queue deckUpdatesQueue() {
-        return new Queue("deck_updates_queue", true);
+        return new Queue(deckUpdatesQueueName, true);
     }
-
 }
--- magic-main/src/main/java/br/com/api/magic/config/WebSocketConfig.java
+++ magic-main/src/main/java/br/com/api/magic/config/WebSocketConfig.java
@@ -9,14 +9,22 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private static final String BROKER_PREFIX = "/topic";
+    private static final String APP_DESTINATION_PREFIX = "/app";
+    private static final String ENDPOINT = "/ws";
+    private static final String[] ALLOWED_ORIGINS = {"http://localhost:3000", "http://my-frontend.com"};
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker(BROKER_PREFIX);
+        registry.setApplicationDestinationPrefixes(APP_DESTINATION_PREFIX);
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint(ENDPOINT)
+                .setAllowedOrigins(ALLOWED_ORIGINS)
+                .withSockJS();
     }
 }
